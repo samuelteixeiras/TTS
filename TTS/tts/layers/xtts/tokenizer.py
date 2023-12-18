2c109db8690f416718296781db65548831fdee1a
Samuel Teixeira <samuelteixeiras@gmail.com>
2023-12-18 00:17:38 +0000

    change limits

--- TTS/tts/layers/xtts/tokenizer.py
+++ TTS/tts/layers/xtts/tokenizer.py
@@ -595,12 +595,12 @@ class VoiceBpeTokenizer:
         if vocab_file is not None:
             self.tokenizer = Tokenizer.from_file(vocab_file)
         self.char_limits = {
-            "en": 250,
+            "en": 5000,
             "de": 253,
             "fr": 273,
             "es": 239,
             "it": 213,
-            "pt": 203,
+            "pt": 5000,
             "pl": 224,
             "zh": 82,
             "ar": 166,
